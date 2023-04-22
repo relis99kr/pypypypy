ecf85797f57dc9604c44cbd45012aff67e84145b
relis99kr <128214670+relis99kr@users.noreply.github.com>
2023-04-22 09:22:30 +0000

    update

--- codee/hello.py
+++ codee/hello.py
@@ -1,1 +1,15 @@
-print(list('python'))
+PM = float(input("미세면지량 >> "))
+
+if 151 <= PM:
+    warn = '매우 나쁨'
+
+elif 81 <= PM:
+    warn = "나쁨"
+
+elif 31 <= PM:
+    warn = "보통"
+
+else:
+    warn = "좋음"
+
+print(f'미세먼지 농도: {PM:.2f}, 등급 {warn}')
